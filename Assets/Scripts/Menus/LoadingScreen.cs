92529bf6d3d2631889bc3bee7a88956463f31e16
Peyton Paul <peyton.z.paul@gmail.com>
2022-04-02 13:35:18 -0400

    --- Auto-Git Commit ---

--- Assets/Scripts/Menus/LoadingScreen.cs
+++ Assets/Scripts/Menus/LoadingScreen.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LoadingScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Loading Screen Variables")]
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+    public Slider loadingBar;
+
+    //Function to turn the load screen on and start a load level coroutine
+    public void LoadScene(string levelToLoad)
     {
-        
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadLevelCo(levelToLoad));
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator LoadLevelCo(string levelToLoad)
     {
-        
+        //Create a AsyncOperation to loading a scene and make it so it wont load into scene yet
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelToLoad);
+        operation.allowSceneActivation = false;
+
+        //While loop while game is loading
+        while (!operation.isDone)
+        {
+            //Make the value of the slider = to the progress
+            loadingBar.value = operation.progress;
+
+            //If the operation progress gets over 9 then allow for input and change up the text
+            if (operation.progress > -.9f)
+            {
+                loadingText.text = "Press any Key to Continue";
+
+                if (Input.anyKeyDown && !operation.allowSceneActivation)
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
+            else
+                //Make the loading text display this text when loading
+                loadingText.text = "Loading...";
+
+            yield return null;
+        }
     }
+
+
 }

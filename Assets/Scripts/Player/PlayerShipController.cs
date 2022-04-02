fb7e8f11a7d0c01abb19216c0adaed43a5ac14e6
Tyler <gordontwatts@gmail.com>
2022-04-02 01:17:53 -0400

    Player movement improvement

--- Assets/Scripts/Player/PlayerShipController.cs
+++ Assets/Scripts/Player/PlayerShipController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerShipController : MonoBehaviour
 {
-    private float moveSpeed = 5f;
+    private float thrust = 1f;
+    private float turnSpeed = 20f;
     public PlayerInput input;
     public Rigidbody2D rb2d;
 
@@ -21,7 +22,20 @@ public class PlayerShipController : MonoBehaviour
 
     private void Move(Vector2 movement)
     {
-        Debug.Log(movement);
-        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // Turning right/left
+        if(movement.x != 0f)
+        {
+            transform.Rotate(new Vector3(0f, 0f, -movement.x) * turnSpeed * Time.fixedDeltaTime);
+        }
+
+        // Thrust up
+        if (movement.y > 0f)
+        {
+            rb2d.AddForce(transform.right * thrust, ForceMode2D.Impulse);
+        }
+        if (movement.y < 0f) // Thrust down
+        {
+            rb2d.AddForce(-transform.right * thrust, ForceMode2D.Impulse);
+        }
     }
 }

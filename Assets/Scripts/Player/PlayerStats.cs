a21a8ae33dceb9d819867e08899ec551e70f6653
Austin Smith <pairjax@protonmail.com>
2022-04-02 19:48:51 -0400

    Updated PlayerStats
    - Added upgrade variables.

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -7,4 +7,12 @@ public class PlayerStats : MonoBehaviour
     public int dubloons = 0;
     public int gotcha = 0;
     public int maxGotcha = 100;
+    public int beamCount = 1;
+
+    // Booties
+    public int speedUp = 0;
+    public int damageUp = 0;
+    public int healthUp = 0;
+    public int beamLengthUp = 0;
+    public int beamPlus = 0;
 }

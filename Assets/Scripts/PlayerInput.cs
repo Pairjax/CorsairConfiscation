3c162fe9a7d433472fefc4a5480935567694733c
Austin Smith <pairjax@protonmail.com>
2022-04-01 20:16:48 -0400

    Basic Player Input
    - Simple input script to capture the current input.
    - Holds the gamestate, but it may be better to track that somewhere else.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    enum GameState
+    {
+        FLYING,
+        SHOP,
+        PAUSED
+    }
+
+    // TODO: Pause menu
+    private GameState currentState = GameState.FLYING;
+
+    public Vector2 movementInput { get; private set; }
+
+    public bool tractorActive { get; private set; }
+
+    public bool paused { get; private set; }
+
+    void Update()
+    {
+        if (currentState == GameState.FLYING) handleGameInput();
+        else handleMenuInput();
+    }
+
+    private void handleGameInput()
+    {
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        tractorActive = Input.GetButton("Spacebar");
+    }
+
+    private void handleMenuInput()
+    {
+        // TODO: Whatever goes in here
+    }
+}
